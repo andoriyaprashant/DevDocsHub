6e5054768f57ccc530297c6670bb1a5343fcdbcd
Om Jadhav <om427880@gmail.com>
2024-10-02 21:14:18 +0530

    Added the forms to add documentations and languages also detail view of documentations

--- DevDocsHub/urls.py
+++ DevDocsHub/urls.py
@@ -22,6 +22,7 @@ Including another URLconf
 # ]
 
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
     path('', include('Docs.urls')),
--- Docs/views.py
+++ Docs/views.py
@@ -1,11 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import ProgrammingLanguage, Documentation
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages
 
 def home(request):
     languages = ProgrammingLanguage.objects.all()
     return render(request, 'home.html', {'languages': languages})
 
-def documentation_list(request, language_id):
-    language = ProgrammingLanguage.objects.get(id=language_id)
-    docs = Documentation.objects.filter(language=language)
-    return render(request, 'docs_list.html', {'language': language, 'docs': docs})
+def documentation_list(request):
+    language = ProgrammingLanguage.objects.all()
+    docs = Documentation.objects.all()
+    if request.method=="POST":
+        got_language = request.POST.get('language','')
+        if language:
+            sort_language = ProgrammingLanguage.objects.filter(name=got_language).first()
+
+            if sort_language:
+                docs = Documentation.objects.filter(language=sort_language)
+    
+    context={
+        'language': language,
+        'docs': docs
+    }
+    return render(request, 'docs_list.html',context)
+
+
+def add_documentation(request):
+    if request.method == 'POST':
+        p_language = request.POST.get('language', '')
+        doc_title = request.POST.get('doc_title', '')
+        content = request.POST.get('content', '')
+        doc_url = request.POST.get('url', '')
+
+
+        try:
+            language = ProgrammingLanguage.objects.get(name=p_language)
+            Documentation.objects.create(
+                language=language,
+                doc_title=doc_title,
+                content=content,
+                doc_url=doc_url
+            )
+            return redirect('home')
+
+        except ObjectDoesNotExist:
+            return render(request, 'error.html', {'error': 'Programming language not found'})
+        
+    all_language=ProgrammingLanguage.objects.all()
+
+    context={
+        'all_language':all_language,
+    }
+    return render(request, 'Add_documentation.html',context)
+
+
+
+def Add_languages(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        official_website = request.POST.get('url', '')
+        description = request.POST.get('content', '')
+        
+        try:
+            ProgrammingLanguage.objects.create(
+                name=name,
+                official_website=official_website,
+                description=description
+            )
+            messages.success(request, "Programming language added successfully!")
+            return redirect('home')
+        except Exception as e:
+            messages.error(request, f"An error occurred: {e}")
+    
+    return render(request, 'Add_language.html')
