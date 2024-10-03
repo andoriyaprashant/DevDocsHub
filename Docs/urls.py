e5c14e77d8fc3a7eaa27c8081f45494c576c0dee
Aditya-Mishra9670 <codingbyaditya2005@gmail.com>
2024-10-03 11:46:31 +0530

    Improve home page with better dark mode option and better navbaar with organized dropdown action in navbar that can be extended with more features when needed

--- Docs/urls.py
+++ Docs/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('docs/', views.docs_list, name='docs_list'),
     path('language/<int:language_id>/', views.documentation_list, name='documentation_list'),
 ]
 
