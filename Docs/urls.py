bcc53b9474a0d5d65178f88276f6e9177d98f81e
abhijeet <abhijeet.kumar@m2crowd.com>
2024-10-14 22:28:39 +0530

    added sesarch

--- Docs/urls.py
+++ Docs/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('language/<int:language_id>/', views.documentation_list, name='documentation_list'),
+    path('search/', views.search_language, name='search_language'),
 ]
 
