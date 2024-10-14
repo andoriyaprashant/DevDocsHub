0a43b1f7647e251e6a64f52b54efb952e606de7c
abhijeet <abhijeet.kumar@m2crowd.com>
2024-10-14 22:21:31 +0530

    adedd json data and cmd in redme file

--- Docs/urls.py
+++ Docs/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('language/<int:language_id>/', views.documentation_list, name='documentation_list'),
-    path('search/', views.search_language, name='search_language'),
 ]
 
--- Docs/views.py
+++ Docs/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from .models import ProgrammingLanguage, Documentation
-from django.shortcuts import get_object_or_404, redirect
-from django.http import JsonResponse
 
 def home(request):
     languages = ProgrammingLanguage.objects.all()
@@ -12,12 +10,5 @@ def documentation_list(request, language_id):
     docs = Documentation.objects.filter(language=language)
     return render(request, 'docs_list.html', {'language': language, 'docs': docs})
 
-def search_language(request):
-    query = request.GET.get('name', '')
-    if query:
-        languages = ProgrammingLanguage.objects.filter(name__icontains=query)
-        if languages.exists():
-            language = languages.first()
-            return redirect(language.official_website)
-    return JsonResponse({'error': 'No programming language found'}, status=404)
+
 
