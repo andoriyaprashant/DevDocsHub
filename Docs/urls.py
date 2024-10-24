5dec367b254d45dc7bb312c59e6de4ad75988a78
Kushagra Vardhan <kushagravardhan619@gmail.com>
2024-10-24 23:12:57 +0530

    Added Languages in the progamming languages Section

--- Docs/urls.py
+++ Docs/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/',views.contact,name='contact'),
+    path('contact/', views.contact, name='contact'),
     path('language/<int:language_id>/', views.documentation_list, name='documentation_list'),
     path('search/', views.search_language, name='search_language'),
+    path('programming_languages_info/', views.programming_languages_info, name='programming_languages_info'),
 ]
-
--- Docs/views.py
+++ Docs/views.py
@@ -3,14 +3,17 @@ from .models import ProgrammingLanguage, Documentation
 from django.shortcuts import get_object_or_404, redirect
 from django.http import JsonResponse
 
+
 def home(request):
     languages = ProgrammingLanguage.objects.all()
     return render(request, 'home.html', {'languages': languages})
+
 
 def documentation_list(request, language_id):
     language = ProgrammingLanguage.objects.get(id=language_id)
     docs = Documentation.objects.filter(language=language)
     return render(request, 'docs_list.html', {'language': language, 'docs': docs})
+
 
 def search_language(request):
     query = request.GET.get('name', '')
@@ -21,9 +24,14 @@ def search_language(request):
             return redirect(language.official_website)
     return JsonResponse({'error': 'No programming language found'}, status=404)
 
+
 def about(request):
     return render(request, 'html/about.html')
+
 
 def contact(request):
     return render(request, 'html/contact_us.html')
 
+
+def programming_languages_info(request):
+    return render(request, 'programming_languages_info.html')
