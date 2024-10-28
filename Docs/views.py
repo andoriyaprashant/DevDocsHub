f4b9bdf4b0e0b91c2da73fb2a3448542eebf5099
Baig-fatema <mirzafatmabaig1012@gmail.com>
2024-10-28 10:03:54 +0530

    resolve BUG: CSRF Token Error on Contact Us Page #134

--- Docs/views.py
+++ Docs/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import ProgrammingLanguage, Documentation
+from .models import ProgrammingLanguage, Documentation,Contact
 from django.shortcuts import get_object_or_404, redirect
 from django.http import JsonResponse
-
+from django.contrib import messages
 
 def home(request):
     languages = ProgrammingLanguage.objects.all()
@@ -35,3 +35,22 @@ def contact(request):
 
 def programming_languages_info(request):
     return render(request, 'programming_languages_info.html')
+
+
+#code for handling contact us form data
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        query = request.POST.get('query')
+
+        # You can also add basic validation here if needed
+        if name and email and query:
+            contact = Contact(name=name, email=email, query=query)
+            contact.save()  # Save the contact data to the database
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contact')  # Redirect to the contact page or a success page
+        else:
+            messages.error(request, 'Please fill out all fields.')
+
+    return render(request, 'html/contact_us.html')
